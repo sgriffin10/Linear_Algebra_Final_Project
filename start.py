bd231063ab677cb99be0792299fb7eec2778570e
sgriffin10 <sgriffin4@babson.edu>
2020-04-27 20:19:55 -0400

    start changes

--- start.py
+++ start.py
@@ -12,9 +12,3 @@ if 'ipython' in globals():
 # Display all cell outputs in notebook
 from IPython.core.interactiveshell import InteractiveShell
 InteractiveShell.ast_node_interactivity = 'all'
-
-# Visualization
-# import plotly.plotly as py
-# import plotly.graph_objs as go
-# from plotly.offline import iplot, init_notebook_mode
-# init_notebook_mode(connected=True)
