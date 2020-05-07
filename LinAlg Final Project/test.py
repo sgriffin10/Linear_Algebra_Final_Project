91601b6a4e1a521c4524b1beb7546ab3763b377f
sgriffin10 <sgriffin4@babson.edu>
2020-05-07 17:38:38 -0400

    final project formatting changes

--- LinAlg Final Project/test.py
+++ LinAlg Final Project/test.py
@@ -1,7 +1,9 @@
+mod_input = int(input("For mod 26, type '26'. For mod 29, type '29': "))
 
-for determinant in range(-1,-26,-1):
-    for i in range(1, 26):
-        if (abs((determinant * i) % 26) == 1):
+
+for determinant in range(0,26):
+    for i in range(0, 26):
+        if (abs((determinant * i) % mod_input) == 1):
             # new_scalar = i
             print("The determinant is: {} and the scalar is {}".format(determinant,i))
             # reverse_matrix = (new_scalar * new_matrix) % 26
@@ -9,7 +11,3 @@ for determinant in range(-1,-26,-1):
             # print("There is no mod.")
             continue
 
-# print(abs(-4))
-
-# for i in range(-1,-26,-1):
-#     print(i)
